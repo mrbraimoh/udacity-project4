1606b5310cbef3432131a914fac38903bc542869
PETER BRAIMOH <peterbraimoh98@gmail.com>
2022-10-12 22:26:37 +0100

    first commit

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'qxww5fblcb';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/prod`
+
+export const authConfig = {
+  domain: 'dev-nou5d7vz.us.auth0.com',
+  clientId: 'OK1uvYvJ6S04lsJ93xKj9qiD1CHbrUwL',
+  callbackUrl: 'http://localhost:3000/callback'
+};
